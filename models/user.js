f18348cff1cbc8830c962bd4eaf8724a1f31c482
Caleb Knight <knightca@oregonstate.edu>
2023-05-19 12:22:11 -0700

    Use bcrypt to hash and salt password for user

--- models/user.js
+++ models/user.js
@@ -2,13 +2,16 @@ const { DataTypes } = require('sequelize')
 
 const sequelize = require('../lib/sequelize')
 
+const bcrypt = require('bcryptjs');
+
+
 const User = sequelize.define('user', {
   userId: { type: DataTypes.INTEGER, allowNull: false },
   name: { type: DataTypes.STRING, allowNull: false },
   email: { type: DataTypes.STRING, allowNull: false },
-  password: { type: DataTypes.STRING, allowNull: false },
+  password: { type: DataTypes.STRING, allowNull: false, async set(value) { const pwHash = await bcrypt.hash(User.password, 8); this.setDataValue('password', hash(value)); } },
   admin: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false }
-})
+});
 
 exports.User = User
 exports.UserClientFields = [
